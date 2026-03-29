9f569e349b2526fefb2d55288167b32722c9b82f
sonamenil <omaremil213@gmail.com>
2026-03-29 10:50:50 -0400

    Trick fixes

--- Assets/Scripts/TrickGO.cs
+++ Assets/Scripts/TrickGO.cs
@@ -46,10 +46,10 @@ public class TrickGO : MonoBehaviour
 
     public void UpdatePosition(float w, float h)
     {
-        var vector = new Vector3(w * 0.5f, h * -0.5f);
+        var vector = new Vector3(w * 0.5f, h * 0.5f);
         _animation.transform.localPosition = vector;
 
-        var vector2 = new Vector3(w * 0.5f, h * -0.5f + -23.28f);
+        var vector2 = new Vector3(w * 0.5f, h * 0.5f + 23.28f);
         _trickIcon.transform.localPosition = vector2;
     }
 
