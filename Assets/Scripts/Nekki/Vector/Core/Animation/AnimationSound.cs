fac4fbfb6837065b67b9bc2105e07439f43715a0
kubinka0505 <kubinka0505@yandex.com>
2026-05-17 11:58:36 +0200

    random pitch from 99% to 101%

--- Assets/Scripts/Nekki/Vector/Core/Animation/AnimationSound.cs
+++ Assets/Scripts/Nekki/Vector/Core/Animation/AnimationSound.cs
@@ -12,10 +12,11 @@ namespace Nekki.Vector.Core.Animation
 			_names = new List<string>(name.Split('|'));
 		}
 
-		public void Play(float p_volume = 1f)
+		public void Play(float p_volume = 1f) //, float p_pitch = 1f)
 		{
+			float pitch = Random.Range(0.99f, 1.01f);
 			var num = Random.Range(0, _names.Count);
-			SoundsManager.Instance.PlaySoundsOnce(_names[num], p_volume);
+			SoundsManager.Instance.PlaySoundsOnce(_names[num], p_volume, pitch);
 		}
 	}
 }
